b3ab7c9271a5529baa5dee5b43138dbfcd5a67c5
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-08-01 14:25:06 -0700

    Layout and Home Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def meta_title
+    if @page_title
+      "#{@page_title} | Marina Athena"
+    else
+      "Marina Athena"
+    end
+  end
+
+  def page_heading
+    if @page_title
+      "<h1 class=\"page-heading\">#{@page_title}</h1>"
+    else
+      ""
+    end
+  end
 end
