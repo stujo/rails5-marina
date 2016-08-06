edffa64a3fe6445ccd468654dbc2b62878938ab1
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-08-05 22:27:08 -0700

    Remove duplicate react require

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,3 +1,3 @@
-window.React = require('react');
+var React = window.React = global.React = require('react');
 window.ReactDOM = require('react-dom');
 window.WeatherRoot  = require('./components/containers/WeatherRoot.es6').default;
